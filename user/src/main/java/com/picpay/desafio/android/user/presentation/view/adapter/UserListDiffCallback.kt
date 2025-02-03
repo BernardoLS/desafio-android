b9fed254c0ae5a2c8d24a352c59390b8ac16e4de
Bernardo Laguna dos Santos <bernardolagunasantos@gmail.com>
2025-02-03 00:33:43 -0300

    Remove unecessary test and add test to diff callback

--- user/src/main/java/com/picpay/desafio/android/user/presentation/view/adapter/UserListDiffCallback.kt
+++ user/src/main/java/com/picpay/desafio/android/user/presentation/view/adapter/UserListDiffCallback.kt
@@ -9,18 +9,14 @@ class UserListDiffCallback(
 ) : DiffUtil.Callback() {
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition].username == newList[newItemPosition].username
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
     }
 
-    override fun getOldListSize(): Int {
-        return oldList.size
-    }
+    override fun getOldListSize() = oldList.size
 
-    override fun getNewListSize(): Int {
-        return newList.size
-    }
+    override fun getNewListSize() = newList.size
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return true
+        return oldList[oldItemPosition] == newList[newItemPosition]
     }
 }
